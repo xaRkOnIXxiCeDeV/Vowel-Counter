1532cd30bdc5621721c7d816825279342c1822bd
PeNgUiN_TeCh <97347889+xaRkOnIXxiCeDeV@users.noreply.github.com>
2022-03-12 08:49:40 -0500

    Update Vrai challenge tp.py

--- Vrai challenge tp.py
+++ Vrai challenge tp.py
@@ -1,18 +1,9 @@
-def get_vowels_numbers(word):
-    # créer un compteur de voyelles
+def get_vowels_numbers(wordenter):
     nb_vowels = 0
-
-    # pour chaque lettre du mot vous verifiez s'il s'agit d'un voyelle
     for letter in word:
         if letter in ['a', 'e', 'i', 'o', 'u', 'y']:
-            # on ajoute un au compteur
             nb_vowels += 1
-
-    # à la fin de la fonction, vous allez renvoyer le compteur
     return nb_vowels
-
-
-word = input("Entrer un mot")
+wordenter = input("Enter A Number")
 vowels_count = get_vowels_numbers(word)
-print("Il y a", vowels_count, "voyelles dans le mot", word)
-print("veut tu rejouer")
+print("There Are ", vowels_count, "Number Of Vowels In The Word ", wordenter)
