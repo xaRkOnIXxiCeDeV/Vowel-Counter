c4c3990eee94a7824e721478f7d9b183e317c556
PeNgUiN_TeCh <97347889+xaRkOnIXxiCeDeV@users.noreply.github.com>
2022-03-12 08:55:38 -0500

    Add files via upload

---
+++ Vowels_Counter.py
@@ -0,0 +1,9 @@
+def get_vowels_numbers(wordenter):
+    nb_vowels = 0
+    for letter in wordenter:
+        if letter in ['a', 'e', 'i', 'o', 'u', 'y']:
+            nb_vowels += 1
+    return nb_vowels
+wordenter = input("Enter A Number")
+vowels_count = get_vowels_numbers(wordenter)
+print("There Are ", vowels_count, "Number Of Vowels In The Word ", wordenter)
