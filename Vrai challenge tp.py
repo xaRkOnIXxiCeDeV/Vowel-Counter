fa401ba0895da6ef852b3100823bc626b33cf110
PeNgUiN_TeCh <97347889+xaRkOnIXxiCeDeV@users.noreply.github.com>
2022-03-12 08:56:00 -0500

    Delete Vrai challenge tp.py

--- Vrai challenge tp.py
+++
@@ -1,9 +0,0 @@
-def get_vowels_numbers(wordenter):
-    nb_vowels = 0
-    for letter in word:
-        if letter in ['a', 'e', 'i', 'o', 'u', 'y']:
-            nb_vowels += 1
-    return nb_vowels
-wordenter = input("Enter A Number")
-vowels_count = get_vowels_numbers(word)
-print("There Are ", vowels_count, "Number Of Vowels In The Word ", wordenter)
